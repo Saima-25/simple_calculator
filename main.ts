84e0b05a88aa7580e020630f82ba4dd676ec3829
unknown <maima.alive25@gmail.com>
2024-03-24 22:18:54 +0500

    push my calculator to github.

---
+++ main.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+// Shabang
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "enter frist number", type: "number", name: "firstNumber" },
+  { message: "enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of these operator to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+  },
+]);
+if(answer.operator==="Addition"){
+console.log( answer.firstNumber + answer.secondNumber);
+}
+else if(answer.operator==="Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if(answer.operator==="Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if(answer.operator==="Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else{
+    console.log("select one of above operator")
+}
